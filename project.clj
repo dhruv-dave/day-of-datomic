3d2e71c7f4f0a946f4393179e96323e559470ac0
Shwetank Dave <shwetank.dave@cenx.com>
2014-08-07 14:33:03 -0400

    [Issue #1] updating datomic to 4815.12

--- project.clj
+++ project.clj
@@ -3,6 +3,13 @@
   :plugins [[lein-tg "0.0.1"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.generative "0.3.0"]
-                 [com.datomic/datomic-free "0.9.4755"]
+                 [com.datomic/datomic-pro "0.9.4815.12"
+                  :exclusions [org.slf4j/slf4j-nop org.slf4j/slf4j-log4j12]]
                  [incanter/incanter-charts "1.3.0"]
-                 [incanter/incanter-pdf "1.3.0"]])
+                 [incanter/incanter-pdf "1.3.0"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [midje "1.6.3"]
+                                  [rhizome "0.2.0"]]}}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}})
